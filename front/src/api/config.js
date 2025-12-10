3644a49b7e4e54a214bd3f435135b06782453f7d
ingvionio <andanteloos74@gmail.com>
2025-12-11 03:08:30 +0500

    Исправил конфиг 3

--- front/src/api/config.js
+++ front/src/api/config.js
@@ -1,6 +1,6 @@
 // Базовый URL API
 // В продакшене замените на реальный URL вашего бекенда
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:82';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://localhost:81';
 
 export default API_BASE_URL;
 
