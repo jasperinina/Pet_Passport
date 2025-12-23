8e3b6b84df11486f83ed68d15e5f348dbb736268
ingvionio <andanteloos74@gmail.com>
2025-12-23 13:51:53 +0500

    Убрал маркеры

--- front/src/components/ui/ProcedureCard.jsx
+++ front/src/components/ui/ProcedureCard.jsx
@@ -4,21 +4,6 @@ import BellOnIcon from "../../assets/icons/icon-notifications-on.svg";
 import BellOffIcon from "../../assets/icons/icon-notifications-off.svg";
 
 import DateIcon from "../../assets/icons/icons-date.svg";
-import { EVENT_TYPES } from "../../constants/eventConstants";
-
-// Функция для получения эмодзи и цвета маркера по типу события
-const getEventMarker = (eventType) => {
-  switch (eventType) {
-    case EVENT_TYPES.DOCTOR_VISIT:
-      return { emoji: "🩺", color: "#4A90E2" }; // Синий для приема врача
-    case EVENT_TYPES.VACCINE:
-      return { emoji: "💉", color: "#50C878" }; // Зеленый для вакцинации
-    case EVENT_TYPES.TREATMENT:
-      return { emoji: "🧪", color: "#FF6B6B" }; // Красный для обработки
-    default:
-      return { emoji: "📋", color: "#9B9B9B" }; // Серый по умолчанию
-  }
-};
 
 const ProcedureCard = ({
   title,
@@ -26,7 +11,6 @@ const ProcedureCard = ({
   time,
   fullDate,
   typeName,
-  eventType,
   reminderEnabled,
   onClick,
 }) => {
@@ -38,21 +22,12 @@ const ProcedureCard = ({
     ? "Напоминание включено"
     : "Напоминание выключено";
 
-  const marker = getEventMarker(eventType);
 
   return (
     <div className="procedure-card" onClick={onClick}>
       <div className="procedure-card__left">
         <div className="procedure-card__top">
-          {/* Визуальный маркер типа события */}
-          <div 
-            className="procedure-card__type-marker"
-            style={{ backgroundColor: marker.color }}
-            title={typeName}
-          >
-            <span className="procedure-card__type-emoji">{marker.emoji}</span>
-          </div>
-
+        
           <img
             src={bellIcon}
             alt={bellAlt}
